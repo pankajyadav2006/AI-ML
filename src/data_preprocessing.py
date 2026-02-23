d78facd5410e7fc40de626eb8f004a3ed6c0579e
pankajyadav2006 <pankaj.yadav@adypu.edu.in>
2026-02-23 19:51:10 +0530

    Add load_data function to read and clean CSV input

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -7,3 +7,12 @@ import os
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'data', 'yield_df.csv')
 MODELS_DIR = os.path.join(os.path.dirname(__file__), '..', 'models')
+
+
+def load_data(path=None):
+    if path is None:
+        path = DATA_PATH
+    df = pd.read_csv(path)
+    if 'Unnamed: 0' in df.columns:
+        df.drop(columns=['Unnamed: 0'], inplace=True)
+    return df
